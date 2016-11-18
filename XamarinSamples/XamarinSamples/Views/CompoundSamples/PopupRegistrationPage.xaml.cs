2442f2de45fa7d12ef4cffe49837cc64ec9507fd
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2016-11-18 19:57:44 +0530

    committing some modifications

--- XamarinSamples/XamarinSamples/Views/CompoundSamples/PopupRegistrationPage.xaml.cs
+++ XamarinSamples/XamarinSamples/Views/CompoundSamples/PopupRegistrationPage.xaml.cs
@@ -13,12 +13,31 @@ namespace XamarinSamples
 		public PopupRegistrationPage()
 		{
 			InitializeComponent();
-			dynamicRegdata = new ObservableCollection<RegistrationDetails>();
+			dynamicRegdata = new ObservableCollection<RegistrationDetails>()
+			{
+				new RegistrationDetails{firstName="df", lastName="dfs", age="34", email="23234dfdf34r", mobile="56464145968", states="dfsds", cities="dfeff" }
+			};
 
 			registeredData.ItemsSource = dynamicRegdata;
 			registeredData.ItemSelected += (object sender, SelectedItemChangedEventArgs e) =>
 			{
-				var data = ((ListView)sender).SelectedItem;
+				var data = ((ListView)sender).SelectedItem as RegistrationDetails;
+				if (data == null)
+				{
+					return;
+				}
+
+				fNameDisplay.Text = data.firstName;
+				lNameDisplay.Text = data.lastName;
+				ageDisplay.Text = data.age;
+				mobile.Text = data.mobile;
+				emailDisplay.Text = data.email;
+				stateDisplay.Text = data.states;
+				cityDisplay.Text = data.cities;
+
+				overlayDisplayRegForm.IsVisible = true;
+				holder.IsEnabled = false;
+				((ListView)sender).SelectedItem = null;
 			};
 		}
 
@@ -27,12 +46,36 @@ namespace XamarinSamples
 			overlayRegForm.IsVisible = true;
 			holder.IsEnabled = false;
 		}
-
+		public void ClearForm(object sender, EventArgs args)
+		{
+			ClearFormData();
+		}
+		public void SubmitForm(object sender, EventArgs args)
+		{
+			ClearFormData();
+		}
 		public void CloseRegForm(object sender, EventArgs args)
 		{
+			ClearFormData();
 			overlayRegForm.IsVisible = false;
 			holder.IsEnabled = true;
 		}
+		public void ClearFormData()
+		{
+			fNameEntry.Text = string.Empty;
+			lNameEntry.Text = string.Empty;
+			ageEntry.Text = string.Empty;
+			mobileEntry.Text = string.Empty;
+			emailEntry.Text = string.Empty;
+			stateEntry.Text = string.Empty;
+			cityEntry.Text = string.Empty;
+		}
+		public void CloseDisplayForm(object sender, EventArgs args)
+		{
+			overlayDisplayRegForm.IsVisible = false;
+			holder.IsEnabled = true;
+		}
+
 
 		//public void AddNefw(object sender, EventArgs args)
 		//{
